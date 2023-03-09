101a30fbd270c6df9ad2d75cc7e586566070c312
Dixita Patel <dixita.wappnet@gmail.com>
2023-03-09 19:46:28 +0530

    React-app using Tailwind Css

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import {HiOutlineBell,HiOutlineLogout,HiOutlineSearch,HiOutlineCog} from 'react-icons/hi'
-
+import { Menu } from '@headlessui/react';
 
 export default function Header() {
   return (
-    <div className="bg-white h-16 px-4 flex justify-between items-center text-xl">
+    <div className="h-16 px-4 flex justify-between items-center text-xl">
       <div><strong>Create New Tour</strong></div>
       <div className='flex items-center gap-3 mr-2'>
         <HiOutlineBell/>  
         <HiOutlineSearch/>
         <HiOutlineCog/>
         <HiOutlineLogout/>
-        <div className='' style={{backgroundImage:'url(./assets/face.png)'}}>
+        <Menu>
+          <Menu.Button>
+           <div className='h-10 w-10 rounded-full bg-sky-500 bg-cover' style={{backgroundImage:'url(./assets/face.png)'}}></div> 
+          </Menu.Button>
+        </Menu>
             
         </div>
        </div>
-    </div> 
+    
   );
 }
